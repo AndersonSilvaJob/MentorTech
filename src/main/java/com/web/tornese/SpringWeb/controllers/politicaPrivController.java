d63b64d4302f7b2105341c2c6b5c3ce43e6e779a
Carlos H L Santos <64669408+carloshenyk@users.noreply.github.com>
2022-06-13 19:42:19 -0300

    penutimo commit

--- src/main/java/com/web/tornese/SpringWeb/controllers/politicaPrivController.java
+++ src/main/java/com/web/tornese/SpringWeb/controllers/politicaPrivController.java
@@ -4,18 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
-public class InicioController {
-   
-    @GetMapping("/iniciocadastro")
-    public String inicio(){
-      return "site/escolhacadastro";
+public class politicaPrivController {
+    
+    @GetMapping(value="/politicaPriv")
+    public String politicaPriv() {
+        return "site/politicaPriv";
     }
     
-    @GetMapping("/iniciologin")
-    public String iniciologin(){
-      return "site/escolhalogin";
-    }
-
 }
